77a0e157d7e45688c55347e89a42e056d6b2bc28
grifting-goat <137014275+grifting-goat@users.noreply.github.com>
2025-06-16 11:10:30 -0400

    everything is broken

--- main.ts
+++ main.ts
@@ -1,7 +1,33 @@
-namespace tester {
-    export function test(x: number, y:number): void {
-        led.plot(x,y)
+/**
+ * These should be the blocks required for students to interact with the s4
+ * by MakeCode
+ */
+
+//% color="#4beb36"
+namespace S4comms {
+
+    /**
+     * This should be placed in the start up section
+     */
+    //% block
+    export function init() {
+
+    }
+
+    /**
+     * This should be placed within your main loop
+     */
+    //% block
+    export function downlink(id: number, temp: number, data1: number, data2: number, data3: number) {
+
     }
 
 
+    /**
+     * This is an event handler block with no current use
+     */
+    //% block="on event"
+    export function onEvent(handler: () => void) {
+
+    }
 }
