f189d7c55f9f90747a869e8aad0c87da7fcb15c0
grifting-goat <137014275+grifting-goat@users.noreply.github.com>
2025-06-16 12:31:40 -0400

    fixed commit issues

--- main.py
+++ main.py
@@ -1,3 +1,73 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+"""
+
+These should be the blocks required for students to interact with the s4
+by MakeCode
+
+"""
+# % color="#4beb36"
+@namespace
+class S4comms:
+    default_channel = 23
+    default_power = 7
+    student_id = 0xFF
+    payloadInterval = 10000
+    intervalTime = input.running_time()
+    """
+    
+    Helper function to build the packets we want to send to master micro:bit
+    
+    
+    """
+    def constructPacket(id: number, temp: number, data1: number, data2: number, data3: number):
+        packet = bytearray(8)
+        # single byte id and temp
+        packet.set_number(NumberFormat.UINT8_LE, 0, id)
+        # Byte 0
+        packet.set_number(NumberFormat.INT8_LE, 1, temp)
+        # Byte 1
+        # 3 2 byte shorts
+        packet.set_number(NumberFormat.INT16_LE, 2, data1)
+        # Byte 2-3
+        packet.set_number(NumberFormat.INT16_LE, 4, data2)
+        # Byte 4-5
+        packet.set_number(NumberFormat.INT16_LE, 6, data3)
+        # Byte 6-7
+        return packet
+    """
+    
+    This should be placed in the start up section
+    @param id Payload identifier (0–255)
+    @param frequency The number of milliseconds to wait before sending next packet
+    
+    """
+    # % block="init with id $id sending every $frequency ms"
+    # % weight=90
+    # % group="Radio"
+    # % inlineInputMode=inline
+    def init(id: number, frequency: number):
+        global payloadInterval, student_id
+        payloadInterval = frequency
+        student_id = id
+        radio.set_transmit_power(default_power)
+        radio.set_frequency_band(default_channel)
+        radio.on()
+    """
+    
+    Sends a downlink packet if enough time has passed
+    @param id Payload identifier (0–255)
+    @param temp Temperature to include (-128 to 127)
+    @param data1 First data value, signed short
+    @param data2 Second data value, signed short
+    @param data3 Third data value, signed short
+    
+    """
+    # % block="request downlink with id $id temp $temp data1 $data1 data2 $data2 data3 $data3"
+    # % weight=90
+    # % group="Radio"
+    # % inlineInputMode=inline
+    def downlink(id: number, temp2: number, data1: number, data2: number, data3: number):
+        global intervalTime
+        if input.running_time() - intervalTime >= payloadInterval:
+            packet2 = constructPacket(id, temp2, data1, data2, data3)
+            radio.send_buffer(packet2)
+            intervalTime = input.running_time()
--- test.ts
+++ test.ts
@@ -1,3 +1,16 @@
-
-S4comms.init(1,5000)
-S4comms.downlink(0, 0, 0, 0, 0)
+let data3 = 0
+let data2 = 0
+let data1 = 0
+let id = 1
+S4comms.init(id, 5000)
+basic.showIcon(IconNames.Surprised)
+basic.forever(function () {
+    basic.showString(".")
+    data1 = input.lightLevel()
+    data2 = input.compassHeading()
+    data3 = input.soundLevel()
+    S4comms.downlink(id, input.temperature(), data1, data2, data3)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(200)
+})
